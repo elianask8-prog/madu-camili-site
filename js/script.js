087b5c7559b58b51ec4e2c8ccf782529eb2d13dc
Eliana Rossi <eliana.sk8@gmail.com>
2026-07-20 02:40:29 -0300

    Atualização js

--- js/script.js
+++ js/script.js
@@ -9,8 +9,12 @@ const observer = new IntersectionObserver((entradas) => {
             setTimeout(() => {
 
             elemento.target.classList.add("mostrar");
+            
+            observer.unobserve(elemento.target);
 
-        }, 1000);
+        }, 500);
+
+        
 
 }
 
@@ -22,5 +26,89 @@ const observer = new IntersectionObserver((entradas) => {
 elementos.forEach((elemento) => {
 
     observer.observe(elemento);
+    
 
+
+});
+
+const btnMadu = document.getElementById("btn-madu");
+const btnCamili = document.getElementById("btn-camili");
+const galeria = document.getElementById("galeria");
+
+
+
+const btnWhatsappBanner = document.getElementById("btn-whatsapp-banner");
+const btnWhatsapp = document.getElementById("btn-whatsapp");
+const opcoesWhatsapp = document.querySelector(".opcoes-whatsapp");
+
+const btnInstagram = document.getElementById("btn-instagram");
+const opcoesInstagram = document.querySelector(".opcoes-instagram");
+
+
+const contato = document.getElementById("contato");
+
+
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.querySelector(".menu");
+
+const galeriaMadu = document.querySelector(".galeria-madu");
+const galeriaCamili = document.querySelector(".galeria-camili");
+const btnFechar = document.getElementById("btn-fechar");
+
+function abrirOpcoesWhatsapp(event){
+
+    event.preventDefault();
+
+    opcoesWhatsapp.classList.toggle("oculto");
+
+}
+
+btnWhatsapp.addEventListener("click", abrirOpcoesWhatsapp);
+
+btnWhatsappBanner.addEventListener("click", (event) => {
+
+    event.preventDefault();
+
+    contato.scrollIntoView({
+        behavior: "smooth"
+    });
+
+});
+
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("menu-aberto");
+});
+
+function abrirOpcoesInstagram(event){
+
+    event.preventDefault();
+
+    opcoesInstagram.classList.toggle("oculto");
+
+}
+
+btnInstagram.addEventListener("click", abrirOpcoesInstagram);
+
+btnFechar.addEventListener("click", () => {
+    
+    galeria.classList.add("oculto");
+    galeriaMadu.classList.add("oculto");
+    galeriaCamili.classList.add("oculto");
+});
+
+btnMadu.addEventListener("click", (event) => {
+    event.preventDefault();
+    galeria.classList.remove("oculto");
+    galeriaMadu.classList.remove("oculto");
+    galeriaCamili.classList.add("oculto");
+    galeriaMadu.scrollIntoView({ behavior: "smooth" });
+});
+
+btnCamili.addEventListener("click", (event) => {
+    event.preventDefault();
+    galeria.classList.remove("oculto");
+    galeriaCamili.classList.remove("oculto");
+    galeriaMadu.classList.add("oculto");
+    galeriaCamili.scrollIntoView({ behavior: "smooth" });
 });
